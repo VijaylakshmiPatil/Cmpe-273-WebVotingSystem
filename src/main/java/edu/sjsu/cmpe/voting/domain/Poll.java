662f5ce6ad33a06d24e81216a2798f205b309110
shivarajsp77 <shivarajsp77@gmail.com>
2014-03-15 17:02:26 -0700

    Poll.java updated

--- src/main/java/edu/sjsu/cmpe/voting/domain/Poll.java
+++ src/main/java/edu/sjsu/cmpe/voting/domain/Poll.java
@@ -5,48 +5,47 @@ package edu.sjsu.cmpe.voting.domain;
 
 import java.util.Random;
 
-/**
- * @author Vijaylakshmi
- *
- */
 public class Poll {
 
 	private String question;
     private String uniqueKey;
+    private String answer;
+    private int yesCount;
+    private int noCount;
+    
     static final String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static Random rnd;
     
 /**
-     * @return the title
+     * @return the question
      */
     public String getQuestion() {
     	return question;
     }
 
     /**
-     * @param title
-     *            the title to set
+     * @param question
+     *            the question to set
      */
     public void setQuestion(String question) {
     	this.question = question;
     }
 
 /**
-     * @return the title
+     * @return the Unique Key
      */
     public String getUniqueKey() {
     	return uniqueKey;
     }
 
     /**
-     * @param title
+     * @param Unique Key
      *            the title to set
      */
     public void setUniqueKey(String uniqueKey) {
     	this.uniqueKey = uniqueKey;
     }
-    
-    
+      
   
     String randomString(int len)
     {
@@ -56,5 +55,47 @@ public class Poll {
     		sb.append(characters.charAt(rnd.nextInt(characters.length())));
     	return sb.toString();
     }
+
+	/**
+	 * @return the answer
+	 */
+	public String getAnswer() {
+		return answer;
+	}
+
+	/**
+	 * @param answer the answer to set
+	 */
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	/**
+	 * @return the yesCount
+	 */
+	public int getYesCount() {
+		return yesCount;
+	}
+
+	/**
+	 * @param yesCount the yesCount to set
+	 */
+	public void setYesCount(int yesCount) {
+		this.yesCount = yesCount;
+	}
+
+	/**
+	 * @return the noCount
+	 */
+	public int getNoCount() {
+		return noCount;
+	}
+
+	/**
+	 * @param noCount the noCount to set
+	 */
+	public void setNoCount(int noCount) {
+		this.noCount = noCount;
+	}
 
 }
