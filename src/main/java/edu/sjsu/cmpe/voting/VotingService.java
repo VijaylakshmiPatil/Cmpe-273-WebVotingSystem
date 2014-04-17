9e49ac19393228e06671c47ca61326520ffdfd6a
ashwinikhadri <ashwinikhadri@gmail.com>
2014-04-16 17:51:58 -0700

    final commit

--- src/main/java/edu/sjsu/cmpe/voting/VotingService.java
+++ src/main/java/edu/sjsu/cmpe/voting/VotingService.java
@@ -2,16 +2,25 @@ package edu.sjsu.cmpe.voting;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.views.ViewBundle;
 
 import edu.sjsu.cmpe.voting.domain.*;
 import edu.sjsu.cmpe.voting.api.resources.RootResource;
 import edu.sjsu.cmpe.voting.api.resources.VoteResource;
 import edu.sjsu.cmpe.voting.config.VotingServiceConfiguration;
+import edu.sjsu.cmpe.voting.repository.UserRepository;
+import edu.sjsu.cmpe.voting.repository.UserRepositoryInterface;
 import edu.sjsu.cmpe.voting.repository.VotingRepository;
 import edu.sjsu.cmpe.voting.repository.VotingRepositoryInterface;
+import edu.sjsu.cmpe.voting.ui.resources.ViewPollResource;
+import edu.sjsu.cmpe.voting.ui.resources.createPollResource;
+import edu.sjsu.cmpe.voting.ui.resources.mainResource;
 
 public class VotingService extends Service<VotingServiceConfiguration> {
 
@@ -22,6 +31,8 @@ public class VotingService extends Service<VotingServiceConfiguration> {
 	@Override
 	public void initialize(Bootstrap<VotingServiceConfiguration> bootstrap) {
 		bootstrap.setName("Voting-service");
+		bootstrap.addBundle(new ViewBundle());
+		bootstrap.addBundle(new AssetsBundle());
 	}
 
 	@Override
@@ -30,8 +41,11 @@ public class VotingService extends Service<VotingServiceConfiguration> {
 		/**Root API */
 		environment.addResource(RootResource.class);
 		/** Poll APIs */
-		VotingRepositoryInterface voteRepository = new VotingRepository(
-				new ConcurrentHashMap<String, Poll>());
+		VotingRepositoryInterface voteRepository = new VotingRepository();
+		//UserRepositoryInterface userRepository = new UserRepository();
 			environment.addResource(new VoteResource(voteRepository));
+			//environment.addResource(new ViewPollResource(voteRepository));
+			environment.addResource(new ViewPollResource(voteRepository));
+			//environment.addResource(new mainResource(userRepository));
 	}
 }
